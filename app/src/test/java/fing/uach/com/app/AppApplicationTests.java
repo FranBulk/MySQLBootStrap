42e9aeb945920e8ce77b1e32f2b6d623eb6d32b1
FranBulk <149741191+FranBulk@users.noreply.github.com>
2026-02-18 09:44:20 -0600

    Uploading the repo

---
+++ app/src/test/java/fing/uach/com/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package fing.uach.com.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
