05359748525cb16b3f701215263889620aaf8159
FranBulk <149741191+FranBulk@users.noreply.github.com>
2026-02-23 18:30:17 -0600

    Still the issues

--- app/src/main/java/Controller/Main_Controller.java
+++ app/src/main/java/Controller/Main_Controller.java
@@ -1,17 +1,35 @@
 package Controller;
 
+import Models.Orden;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
-public class Main_Controller 
-{
+public class Main_Controller {
+
     @GetMapping("/")
-    public String root(Model model)
-    {
-        model.addAttribute("msg", "Hola");
+    public String index(Model model) {
+
+        // LISTA DE ÓRDENES (MODIFICA AQUÍ)
+        List<Orden> ordenes = new ArrayList<>();
+
+        ordenes.add(new Orden(6, "Orden lista", "Paola"));
+        ordenes.add(new Orden(5, "Orden no lista", "Stefy"));
+        ordenes.add(new Orden(4, "Orden no lista", "Leo"));
+        ordenes.add(new Orden(2, "Orden lista", "Julio"));
+        ordenes.add(new Orden(13, "Orden no lista", "Liz"));
+
+        model.addAttribute("ordenes", ordenes);
+
+        // DATOS DE LOS CUADROS
+        model.addAttribute("totalOrdenes", ordenes.size());
+        model.addAttribute("bounceRate", "40%");
+        model.addAttribute("usuarios", 5);
+
         return "index";
     }
-        
 }
--- app/src/main/java/Models/Orden.java
+++ app/src/main/java/Models/Orden.java
@@ -1,13 +1,40 @@
 package Models;
-public class Orden 
-{
-    private int folio = 0;
-    public int getOrden()
-    {
-        return this.folio;
-    }
-    public void setOrden(int folio)
-    {
+
+public class Orden {
+
+    private int folio;
+    private String estado;
+    private String cliente;
+
+    public Orden() {}
+
+    public Orden(int folio, String estado, String cliente) {
         this.folio = folio;
+        this.estado = estado;
+        this.cliente = cliente;
+    }
+
+    public int getFolio() {
+        return folio;
+    }
+
+    public void setFolio(int folio) {
+        this.folio = folio;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
     }
 }
